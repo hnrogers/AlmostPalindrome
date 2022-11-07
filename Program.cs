cf358f7586792fa3747dd11ddc74773357015c3b
Heather <hnrogers10@gmail.com>
2022-11-07 09:52:57 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AP
+{
+    class Primary
+    {
+        public static void Main()
+        {
+            Console.WriteLine("who quests for the twilight dragon");
+        }
+    }
+}
---
+++ StringValidation.cs
@@ -0,0 +1,11 @@
+class StrngValidation
+{
+    public bool AlmostPalindrome(string a)
+    {
+
+        int n = 0;
+        
+        
+        return true; //test
+    }
+}
